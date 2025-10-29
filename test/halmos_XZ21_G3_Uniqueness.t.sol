370380ccb3c75870f96a08a857d032dac6d29a40
shok1122 <shok1122@zohomail.com>
2025-10-29 12:58:43 +0900

    Updated

--- test/halmos_XZ21_G3_Uniqueness.t.sol
+++ test/halmos_XZ21_G3_Uniqueness.t.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
 
-import "./Base_XZ21_Setup.t.sol";
+import {Test} from "forge-std/Test.sol";
+import {XZ21} from "../src/XZ21.sol"; // 置き場所に合わせて調整
 
-contract G3_Uniqueness_XZ21 is Base_XZ21_Setup {
+contract G3_Uniqueness_XZ21 is Test {
     XZ21 internal s;
 
     address constant SM_ADDR  = address(0x1000);
@@ -11,9 +12,14 @@ contract G3_Uniqueness_XZ21 is Base_XZ21_Setup {
     address constant SU_ADDR  = address(0x3000);
 
     function setUp() public {
+        vm.prank(SM_ADDR);
+        s = new XZ21(SP_ADDR);
+
+        vm.prank(SM_ADDR);
+        s.enrollAccount(1, SU_ADDR, hex"01");
     }
 
-    function test_G3_registerFile_NoDuplication (
+    function test_G3_registerFile(
         bytes32 hashVal,
         uint32 otherSplitNum,
         address otherCreator
