edca037998aef4a7307f11eba108e47720f8a337
shok1122 <shok1122@zohomail.com>
2025-10-29 15:21:18 +0900

    Updated

--- test/halmos_XZ21_G5_StateManagement.t.sol
+++ test/halmos_XZ21_G5_StateManagement.t.sol
@@ -1,57 +1,110 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
 
-import "./Base_XZ21_Setup.t.sol";
+import {Test} from "forge-std/Test.sol";
+import {XZ21} from "../src/XZ21.sol";
 
-contract G5_StateManagement_XZ21 is Base_XZ21_Setup {
-    function test_G5_Start2WaitingProof (
-        bytes32 hashVal
-    ) public {
-        vm.assume(hashVal != HASH_VAL);
+contract G5_StateManagement_XZ21 is Test {
+    XZ21 internal s;
 
+    // 役割
+    address constant SM_ADDR  = address(0x1000);
+    address constant SP_ADDR  = address(0x2000);
+    address constant TPA_ADDR = address(0x3000);
+    address constant SU_ADDR  = address(0x4000);
+
+    // 共通テストデータ
+    bytes constant SU_KEY = hex"01";
+    string constant P = "P"; // param.P は string だが内部では string
+    bytes constant G = bytes("G");
+    bytes constant U = bytes("U");
+    bytes32 constant TEST_HASH = keccak256("file");
+    bytes constant TEST_CHAL   = bytes("chal");
+    bytes constant TEST_PROOF  = bytes("proof");
+
+    uint256 time = 0;
+    uint256 failCount = 0;
+
+    enum LastCall { SetChal, SetProof, SetAuditingResult }
+    LastCall public lastCall = LastCall.SetAuditingResult;
+
+    function setUp() public virtual {
+        // SM が SP を指定してデプロイ
+        vm.prank(SM_ADDR);
+        s = new XZ21(SP_ADDR);
+
+        // 初期パラメタ登録（SM のみ）
+        vm.prank(SM_ADDR);
+        s.registerParam(P, G, U);
+
+        // アカウント登録（SM のみ）
+        // accountType==0: TPA, ==1: SU
+        vm.prank(SM_ADDR);
+        s.enrollAccount(0, TPA_ADDR, ""); // TPA 追加
+        vm.prank(SM_ADDR);
+        s.enrollAccount(1, SU_ADDR, SU_KEY);
+
+        // 1. fuzzer がいじる「対象コントラクト」はこのハーネス自身
+        targetContract(address(this));
+
+        // 2. fuzzer が呼んでいい関数 selector 群を登録する
+        bytes4[] memory sels = new bytes4[](3);
+        sels[0] = this.step_setChal.selector;
+        sels[1] = this.step_setProof.selector;
+        sels[2] = this.step_setAuditingResult.selector;
+
+        targetSelector(FuzzSelector({
+            addr: address(this),
+            selectors: sels
+        }));
+    }
+
+    // --- fuzzでランダムに呼ばれる "操作ステップ" 群 ---
+    function step_setChal() public {
         vm.prank(SU_ADDR);
-        s.setChal(hashVal, CHAL);
-
-        XZ21.AuditingLog[] memory logs = s.getAuditingLogs(hashVal);
-        if (logs[0].stage != XZ21.Stages.WaitingProof) {
-            fail();
+        try s.setChal(TEST_HASH, TEST_CHAL) {
+            if (lastCall != LastCall.SetAuditingResult) {
+                failCount++;
+            }
+            lastCall = LastCall.SetChal;
+        } catch {
+            if (lastCall == LastCall.SetAuditingResult) {
+                failCount++;
+            }
         }
     }
 
-    function test_G5_WaitingProof2WaitingResult (
-        bytes32 hashVal
-    ) public {
-        vm.assume(hashVal != HASH_VAL);
-
-        vm.prank(SU_ADDR);
-        s.setChal(hashVal, CHAL);
-
+    function step_setProof() public {
         vm.prank(SP_ADDR);
-        s.setProof(hashVal, PROOF);
-
-        XZ21.AuditingLog[] memory logs = s.getAuditingLogs(hashVal);
-        if (logs[0].stage != XZ21.Stages.WaitingResult) {
-            fail();
+        try s.setProof(TEST_HASH, TEST_PROOF) {
+            if (lastCall != LastCall.SetChal) {
+                failCount++;
+            }
+            lastCall = LastCall.SetProof;
+        } catch {
+            if (lastCall == LastCall.SetChal) {
+                failCount++;
+            }
         }
     }
 
-    function test_G5_WaitingResult2DoneAuditing (
-        bytes32 hashVal
-    ) public {
-        vm.assume(hashVal != HASH_VAL);
-
-        vm.prank(SU_ADDR);
-        s.setChal(hashVal, CHAL);
-
-        vm.prank(SP_ADDR);
-        s.setProof(hashVal, PROOF);
-
+    function step_setAuditingResult() public {
+        time++;
+        vm.warp(time);
         vm.prank(TPA_ADDR);
-        s.setAuditingResult(hashVal, true);
-
-        XZ21.AuditingLog[] memory logs = s.getAuditingLogs(hashVal);
-        if (logs[0].stage != XZ21.Stages.DoneAuditing) {
-            fail();
+        try s.setAuditingResult(TEST_HASH, true) {
+            if (lastCall != LastCall.SetProof) {
+                failCount++;
+            }
+            lastCall = LastCall.SetAuditingResult;
+        } catch {
+            if (lastCall == LastCall.SetProof) {
+                failCount++;
+            }
         }
+    }
+
+    function invariant_G5() public {
+        assertEq(failCount, 0);
     }
 }
